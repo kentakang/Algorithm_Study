24accc754488284506d56d833832c1286252d7c6
Chan Kang <kentakang@icloud.com>
2018-01-20 04:10:43 +0900

    Add LengthofWord.java

---
+++ LengthofWord.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class LengthofWord {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(str.length());
+    }
+}
