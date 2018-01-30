91ebaa5152bdd2890edb386525bdc46dc88a8cd9
Chan Kang <kentakang@icloud.com>
2018-01-30 23:45:10 +0900

    Solved add

---
+++ src/Add.java
@@ -0,0 +1,12 @@
+public class Add {
+    static int add(int param1, int param2) {
+        return param1 + param2;
+    }
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+
+        System.out.println(add(a, b));
+    }
+}
