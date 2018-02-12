37eec456c96903cc2476e2bedf83dd80db7963bf
Chan Kang <kentakang@icloud.com>
2018-02-12 09:47:23 +0900

    Solved Baekjoon 2914

---
+++ src/boj2914/Main.java
@@ -0,0 +1,17 @@
+package boj2914;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int a = Integer.parseInt(st.nextToken());
+        int i = Integer.parseInt(st.nextToken());
+
+        System.out.println((i - 1) * a + 1);
+    }
+}
