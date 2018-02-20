7fec32c4772a435c0b5af8a0e4faf628891653d1
Chan Kang <kentakang@icloud.com>
2018-02-21 08:18:11 +0900

    Solved Baekjoon 2588

---
+++ src/boj2588/Main.java
@@ -0,0 +1,16 @@
+package boj2588;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+
+        System.out.println(a * (b % 10));
+        System.out.println(a * ((b % 100) / 10));
+        System.out.println(a * (b / 100));
+        System.out.println(a * b);
+    }
+}
