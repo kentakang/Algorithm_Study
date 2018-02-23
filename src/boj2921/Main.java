b9b663f2b1349867d38207f8ec4c4792aab9f830
Chan Kang <kentakang@icloud.com>
2018-02-24 00:40:39 +0900

    Solved Baekjoon 2921

---
+++ src/boj2921/Main.java
@@ -0,0 +1,12 @@
+package boj2921;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        System.out.println(n * (n + 1) * (n + 2) / 2);
+    }
+}
