4cc87d149c9aad269d7937213706f9273bbdda40
Chan Kang <kentakang@icloud.com>
2018-03-26 02:11:04 +0900

    Daily - Codeup Problem 1272 Modified

--- Daily_Algorithm/codeup1272.c
+++ Daily_Algorithm/codeup1272.c
@@ -2,19 +2,42 @@
 
 int main()
 {
-    int k, h, a, b;
-
+    int k, h, a = 0, b = 0;
     scanf("%d %d", &k, &h);
 
     if (k % 2 == 0)
-        a = k * 2 - 1;
+    {
+        for (int i = 1; i <= k; i++)
+        {
+            if (i % 2 == 0)
+                a += 10;
+        }
+    }
     else
-        a = (k / 2) * 10;
+    {
+        for (int i = 1; i <= k; i++)
+        {
+            if (i % 2 != 0)
+                a += 1;
+        }
+    }
     
     if (h % 2 == 0)
-        b = h * 2 - 1;
+    {
+        for (int i = 1; i <= h; i++)
+        {
+            if (i % 2 == 0)
+                b += 10;
+        }
+    }
     else
-        b = (h / 2) * 10;
+    {
+        for (int i = 1; i <= h; i++)
+        {
+            if (i % 2 != 0)
+                b += 1;
+        }
+    }
 
     printf("%d", a + b);
 
