4500733ba2d2d89e763b8f61db4de765745b3a3b
Chan Kang <kentakang@icloud.com>
2018-03-26 02:11:16 +0900

    Daily - Codeup Problem 1214 Solved

---
+++ Daily_Algorithm/codeup1214.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int year, month;
+    
+    scanf("%d %d", &year, &month);
+
+    switch (month)
+    {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            printf("31");
+            break;
+        case 2:
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+                printf("29");
+            else
+                printf("28");
+            break;
+        default:
+            printf("30");
+            break;
+    }
+}
