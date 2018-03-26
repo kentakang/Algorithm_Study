e50dff551212e85263f3bec6c13d67c5873e3076
Chan Kang <kentakang@icloud.com>
2018-03-26 18:54:24 +0900

    Daily - Codeup Problem 1286 Solved

---
+++ Daily_Algorithm/codeup1286.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[5];
+    
+    for (int i = 0; i < 5; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("%d\n%d", arr[4], arr[0]);
+}
