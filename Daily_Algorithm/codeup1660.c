b5c0018a895ff0b2b833b9542e8dff524367f69c
Chan Kang <kentakang@icloud.com>
2018-03-27 19:44:39 +0900

    Daily - Codeup Problem 1660 Solved

---
+++ Daily_Algorithm/codeup1660.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[100];
+    scanf("%s", str);
+
+    for (int i = 0; i < strlen(str); i++)
+    {
+        if (str[i] == ',')
+            printf(" ");
+        else
+            printf("%c", str[i]);
+    }
+
+    printf(" ");
+}
