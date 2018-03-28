e648491b36077b3ea4758a48c6a5bf75bcc85d43
Chan Kang <kentakang@icloud.com>
2018-03-28 19:59:45 +0900

    Daily - Codeup Problem 1417 Solved

---
+++ Daily_Algorithm/codeup1417.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[10];
+    
+    for (int i = 0; i < 10; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("%d", arr[7]);
+}
