1a60ad57e91625282b4e3d398b3f9d0740c9413c
Chan Kang <kentakang@icloud.com>
2018-03-30 01:48:11 +0900

    Daily - Codeup Problem 1539 Solved

---
+++ Daily_Algorithm/codeup1539.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int n;
+
+void f(int n); //전역 변수 n과 같은 변수 이름을 사용하지만 서로 다른 변수이다. 
+
+int main()
+{
+  scanf("%d", &n);
+  f(n);
+}
+
+void f(int n)
+{
+    if (n == 0)
+        printf("false");
+    else
+        printf("true");
+}
