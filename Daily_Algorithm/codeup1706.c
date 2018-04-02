84e115f9655529240521d384f2619ce29391822e
Chan Kang <kentakang@icloud.com>
2018-04-02 17:03:01 +0900

    Daily - Codeup Problem 1706 Solved

---
+++ Daily_Algorithm/codeup1706.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[5];
+
+    for (int i = 0; i < 5; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 4; i >= 0; i--)
+    {
+        printf("%d ", arr[i]);
+    }
+}
