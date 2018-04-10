fa3bb145b9d87a6788e77657aedebd365a01eff5
Chan Kang <kentakang@icloud.com>
2018-04-10 20:48:34 +0900

    Solve Codeup Algorithm Problem No. 1569 ~ 1571
    Codeup Algorithm Problem No. 1569 ~ 1571 Solved with C
    
    link : http://codeup.kr/JudgeOnline/problem.php?id=1569
    http://codeup.kr/JudgeOnline/problem.php?id=1570
    http://codeup.kr/JudgeOnline/problem.php?id=1571

---
+++ Daily_Algorithm/codeup1570.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int n, k, d[1010];
+
+int lower_bound(int x)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        if (d[i] >= x)
+            return i;
+    }
+
+    return n + 1;
+}
+
+int main()
+{
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+        scanf("%d", &d[i]);
+
+    scanf("%d", &k);
+
+    printf("%d\n", lower_bound(k));
+}
