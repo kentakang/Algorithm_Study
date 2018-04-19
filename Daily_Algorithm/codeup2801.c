eaa407fc2c6d99592722a7417ad677774d6013a5
Chan Kang <kentakang@icloud.com>
2018-04-19 17:33:03 +0900

    Solve Codeup Algorithm Problem No. 2801 ~ 2802
    Codeup Algorithm Problem No. 2801 ~ 2802 Solved with C
    
    link: http://codeup.kr/JudgeOnline/problem.php?id=2801
    
    http://codeup.kr/JudgeOnline/problem.php?id=2802

---
+++ Daily_Algorithm/codeup2801.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    const int exchange = 1130;
+    int n;
+    scanf("%d", &n);
+    printf("%d", n * exchange);
+}
---
+++ Daily_Algorithm/codeup2802.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    int k, n;
+    scanf("%d %d", &k, &n);
+    printf("%d", n * k);
+}
