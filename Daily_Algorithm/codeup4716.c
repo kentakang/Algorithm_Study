c64a7a41f09d29d9e01707889ba384af60f824bf
Chan Kang <kentakang@icloud.com>
2018-04-21 14:56:33 +0900

    Solve Codeup Algorithm Problem No. 4716
    Codeup Algorithm Problem No. 4716 Solved with C
    
    link: http://codeup.kr/JudgeOnline/problem.php?id=4716

---
+++ Daily_Algorithm/codeup4716.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int sum = 0;
+    int take, getoff, max = 0;
+
+    for (int i = 0; i < 10; i++)
+    {
+        scanf("%d %d", &getoff, &take);
+        sum = sum - getoff + take;
+
+        if (sum > max)
+        {
+            max = sum;
+        }
+    }
+
+    printf("%d", max);
+}
