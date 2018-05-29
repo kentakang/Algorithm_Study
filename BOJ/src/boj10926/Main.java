b0a41ad001ad45657d0f09a417a2890ab94a9ca8
Chan Kang <kentakang@icloud.com>
2018-05-29 22:49:46 +0900

    Solve Baekjoon Algorithm Problem No. 10926
    Baekjoon Algorithm Problem No. 10926 Solved with Java
    
    Problem:
    https://www.acmicpc.net/problem/10926

---
+++ BOJ/src/boj10926/Main.java
@@ -0,0 +1,12 @@
+package boj10926;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+
+        System.out.println(str + "??!");
+    }
+}
