b2cfd250abedea632f092f599e7a3b232090a0de
Chan Kang <kentakang@icloud.com>
2018-10-05 06:25:08 +0900

    Solve JUNGOL Problem No. 1291

---
+++ JUNGOL/src/JUNGOL1291.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class JUNGOL1291 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+        int start = 0, end = 0, k = 0;
+        boolean isLoop = true;
+
+        while (isLoop) {
+            if (k > 0) System.out.println("INPUT ERROR!");
+            start = scanner.nextInt();
+            end = scanner.nextInt();
+            k++;
+
+            if (start > 1 && end > 1) {
+                if (start <= 9 && end <= 9) {
+                    isLoop = false;
+                }
+            }
+        }
+
+        scanner.close();
+
+        if (start > end) {
+            for (int i = start; i >= end; i--) {
+                list.add(i);
+            }
+        } else {
+            for (int i = start; i <= end; i++) {
+                list.add(i);
+            }
+        }
+
+        for (int i = 1; i <= 9; i++) {
+            for (int obj : list) {
+                System.out.printf("%d * %d = %2d   ", obj, i, obj * i);
+            }
+
+            System.out.print("\n");
+        }
+    }
+}
