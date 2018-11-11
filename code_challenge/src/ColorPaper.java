ed7164a6ee411346cb39b998e5abc637a5e5917a
Chan Kang <kentakang@icloud.com>
2018-11-11 17:09:36 +0900

    구름 코드 챌린지 연습문제

---
+++ code_challenge/src/ColorPaper.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class ColorPaper {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCase = Integer.parseInt(br.readLine());
+
+        for (int a = 0; a < testCase; a++) {
+            int[][] paper = new int[100][100];
+            int n = Integer.parseInt(br.readLine());
+            int cnt = 0;
+
+            for (int x = 0; x < n; x++) {
+                StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+                int left = Integer.parseInt(st.nextToken());
+                int bottom = Integer.parseInt(st.nextToken());
+
+                for (int i = left; i < left + 10; i++) {
+                    for (int j = bottom; j < bottom + 10; j++) {
+                        paper[i][j] = 1;
+                    }
+                }
+            }
+
+            for (int i = 0; i < paper.length; i++) {
+                for (int j = 0; j < paper.length; j++) {
+                    if (paper[i][j] == 1)
+                        cnt++;
+                }
+            }
+
+            System.out.println(cnt);
+        }
+    }
+}
---
+++ code_challenge/src/ReadSeries.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class ReadSeries {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] arr = new int[n];
+        boolean isTrue = true;
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (!(arr[i] == arr[i + 1] - 1)) {
+                isTrue = false;
+            }
+        }
+
+        System.out.printf("%s", isTrue ? "YES" : "NO");
+    }
+}
