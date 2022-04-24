275211722c29749829d2c4d249744c8a98694939
Chan Kang <me@kentakang.com>
2022-04-24 19:52:09 +0900

    feat: solve boj5522

---
+++ BOJ/boj5522.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a1, a2, a3, a4, a5 = 0;
+
+    scanf("%d", &a1);
+    scanf("%d", &a2);
+    scanf("%d", &a3);
+    scanf("%d", &a4);
+    scanf("%d", &a5);
+
+    printf("%d", a1 + a2 + a3 + a4 + a5);
+
+    return 0;
+}
