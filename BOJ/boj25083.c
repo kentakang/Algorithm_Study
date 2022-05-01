4d093da8be06344e99c929ccbc2bfad94495957e
Chan Kang <me@kentakang.com>
2022-05-02 00:46:37 +0900

    feat: solve boj25083

---
+++ BOJ/boj25083.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("         ,r'\"7\n"
+           "r`-_   ,'  ,/\n"
+           " \\. \". L_r'\n"
+           "   `~\\/\n"
+           "      |\n"
+           "      |");
+
+    return 0;
+}
